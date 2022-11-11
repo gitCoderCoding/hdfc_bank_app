5864bd6085b21ed5ade413c29e0e8d9bd2922147
ROHIT.M <rohit30061997@gmail.com>
2022-11-11 21:33:36 +0530

    reverting changes

--- Welcome.java
+++ Welcome.java
@@ -2,8 +2,12 @@ class Welcome{
 //Bug Fix-100
 public void hi(){\
    "welcome to badrachalam"//bug 102
+<<<<<<< Updated upstream
 "welcome to badrachalam by rohit"//bug 103
 "Rama"
+=======
+"welcome to badrachalam by rohit.M"//bug 103
+>>>>>>> Stashed changes
   hi peers!!!!!!!
 //logic 
 }
