bb3b7af892fa30aa5567a7093c9b9af3441e731a
ROHIT.M <rohit30061997@gmail.com>
2022-11-11 21:40:46 +0530

    bug 104

--- Welcome.java
+++ Welcome.java
@@ -4,7 +4,7 @@ public void hi(){\
    "welcome to badrachalam"//bug 102
 <<<<<<< Updated upstream
 "welcome to badrachalam by rohit"//bug 103
-"Rama"
+"welcome to rama nama kshetram"//104
 =======
 "welcome to badrachalam by rohit.M"//bug 103
 >>>>>>> Stashed changes
