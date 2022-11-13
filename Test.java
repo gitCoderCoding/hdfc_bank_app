a6469d6bf4d351f17a9d32ffcd1fd6e90d67c3b1
ROHIT.M <rohit30061997@gmail.com>
2022-11-13 21:48:21 +0530

    test file updated

--- Test.java
+++ Test.java
@@ -1,6 +1,11 @@
 class Test{
 	public staic void main(String[] args){
+<<<<<<< Updated upstream
 		System.out.println("HI Peers");
 bug//103
+=======
+		System.out.println("HI Peers");//102
+               
+>>>>>>> Stashed changes
 	}
 }
